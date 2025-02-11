0e8d734ce75b26074737b0bdeb8be0ea8d9927af
caleb1123 <ryy1507@gmail.com>
2025-02-11 18:01:13 +0700

    create update account

--- src/main/java/com/pse/tixclick/service/AccountService.java
+++ src/main/java/com/pse/tixclick/service/AccountService.java
@@ -3,6 +3,7 @@ package com.pse.tixclick.service;
 import com.pse.tixclick.payload.dto.AccountDTO;
 import com.pse.tixclick.payload.request.IntrospectRequest;
 import com.pse.tixclick.payload.request.LoginRequest;
+import com.pse.tixclick.payload.request.UpdateAccountRequest;
 import com.pse.tixclick.payload.response.IntrospectResponse;
 import com.pse.tixclick.payload.response.RefreshTokenResponse;
 import com.pse.tixclick.payload.response.TokenResponse;
@@ -10,4 +11,6 @@ import com.pse.tixclick.payload.response.TokenResponse;
 public interface AccountService {
     boolean changePasswordWithOtp(String email, String password);
     AccountDTO myProfile();
+
+    AccountDTO updateProfile(UpdateAccountRequest accountDTO);
 }
