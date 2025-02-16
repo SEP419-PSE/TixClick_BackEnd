ba753ed504fa0e345a6430d89dca85d559a14f54
caleb1123 <ryy1507@gmail.com>
2025-02-16 10:43:35 +0700

    api get eventActivity by eventId

--- src/main/java/com/pse/tixclick/service/EventActivityService.java
+++ src/main/java/com/pse/tixclick/service/EventActivityService.java
@@ -3,8 +3,14 @@ package com.pse.tixclick.service;
 import com.pse.tixclick.payload.dto.EventActivityDTO;
 import com.pse.tixclick.payload.request.CreateEventActivityRequest;
 
+import java.util.List;
+
 public interface EventActivityService {
     EventActivityDTO createEventActivity(CreateEventActivityRequest eventActivityRequest);
 
     EventActivityDTO updateEventActivity(CreateEventActivityRequest eventActivityRequest, int id);
+
+    boolean deleteEventActivity(int id);
+
+    List<EventActivityDTO> getEventActivityByEventId(int eventId);
 }
