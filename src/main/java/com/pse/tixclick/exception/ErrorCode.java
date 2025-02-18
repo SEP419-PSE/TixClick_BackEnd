6b0f05f1aaab4cd9bbdc6d1fdeabba2d356a32ab
caleb1123 <ryy1507@gmail.com>
2025-02-18 16:22:40 +0700

    api delete member

--- src/main/java/com/pse/tixclick/exception/ErrorCode.java
+++ src/main/java/com/pse/tixclick/exception/ErrorCode.java
@@ -45,6 +45,7 @@ public enum ErrorCode {
     CANNOT_CREATE_MEMBER(1036, "Không thể tạo thành viên", HttpStatus.BAD_REQUEST),
     INVALID_ROLE(1037, "Vai trò không hợp lệ", HttpStatus.BAD_REQUEST),
     MEMBER_NOT_FOUND(1038, "Không tìm thấy thành viên", HttpStatus.NOT_FOUND),
+    COMPANY_INACTIVE(1039, "Công ty chưa được hoạt động", HttpStatus.BAD_REQUEST),
     ;
 
 
