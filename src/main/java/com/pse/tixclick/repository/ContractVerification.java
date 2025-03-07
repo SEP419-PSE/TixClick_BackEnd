ffdbb1a0e1b8b86fe7b779331340c6bab2490c4d
caleb1123 <ryy1507@gmail.com>
2025-03-07 15:57:36 +0700

    update

--- src/main/java/com/pse/tixclick/repository/ContractVerification.java
+++ src/main/java/com/pse/tixclick/repository/ContractVerification.java
@@ -1,6 +1,8 @@
 package com.pse.tixclick.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContractVerification extends JpaRepository<ContractVerification,Integer> {
 }
