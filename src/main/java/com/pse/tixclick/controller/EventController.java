e782552ece8a42bad96654dc9015577e81fbfe83
Dolakien <ngomeo098@gmail.com>
2025-03-11 17:35:00 +0700

    Api event distribution

--- src/main/java/com/pse/tixclick/controller/EventController.java
+++ src/main/java/com/pse/tixclick/controller/EventController.java
@@ -20,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -357,5 +358,10 @@ public class EventController {
                             .result(null)
                             .build());
         }
+    }
+
+    @GetMapping("/category/distribution")
+    public ResponseEntity<Map<String, Double>> getEventCategoryDistribution() {
+        return ResponseEntity.ok(eventService.getEventCategoryDistribution());
     }
 }
