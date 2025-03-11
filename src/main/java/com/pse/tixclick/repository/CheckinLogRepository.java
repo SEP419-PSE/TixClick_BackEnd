ae53eda2320126bc61b69ecf535921c8e3024353
Dolakien <ngomeo098@gmail.com>
2025-03-11 21:16:52 +0700

    Api count checkin

--- src/main/java/com/pse/tixclick/repository/CheckinLogRepository.java
+++ src/main/java/com/pse/tixclick/repository/CheckinLogRepository.java
@@ -2,6 +2,9 @@ package com.pse.tixclick.repository;
 
 import com.pse.tixclick.payload.entity.CheckinLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CheckinLogRepository extends JpaRepository<CheckinLog, Integer> {
+    @Query(value = "SELECT COUNT(*) FROM CheckinLog WHERE checkinStatus = 'CHECKED_IN'")
+    int countTotalCheckins();
 }
