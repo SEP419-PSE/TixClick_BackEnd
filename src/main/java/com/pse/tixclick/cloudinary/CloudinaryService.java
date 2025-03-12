a7be61abcd5eae816fe298a72315421ad0e5067f
caleb1123 <ryy1507@gmail.com>
2025-03-12 17:25:13 +0700

    update api createCompanyDocument

--- src/main/java/com/pse/tixclick/cloudinary/CloudinaryService.java
+++ src/main/java/com/pse/tixclick/cloudinary/CloudinaryService.java
@@ -27,7 +27,7 @@ public class CloudinaryService {
         String fileType = file.getContentType();
 
         // Kiểm tra định dạng file hợp lệ (PDF, DOC, DOCX, TXT)
-        if (fileType == null || !fileType.matches("application/pdf|application/msword|application/vnd.openxmlformats-officedocument.wordprocessingml.document|text/plain")) {
+        if (fileType == null || !fileType.matches("application/pdf")) {
             throw new AppException(ErrorCode.INVALID_FILE_TYPE);
         }
 
