c25d0452c5e18b221a8bb4d71ae732ee25d5b869
Dolakien <ngomeo098@gmail.com>
2025-04-03 00:31:53 +0700

    update checkinTicketPurchase

--- src/main/java/com/pse/tixclick/repository/BackgroundRepository.java
+++ src/main/java/com/pse/tixclick/repository/BackgroundRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface BackgroundRepository extends JpaRepository<Background,Integer> {
 }
