0a33a5e8adc77d64925dc0789f9323c93eb25bd2
caleb1123 <ryy1507@gmail.com>
2025-04-08 16:46:24 +0700

    update subroleMember

--- src/main/java/com/pse/tixclick/service/impl/MemberServiceImpl.java
+++ src/main/java/com/pse/tixclick/service/impl/MemberServiceImpl.java
@@ -239,9 +239,7 @@ public class MemberServiceImpl implements MemberService {
             throw new AppException(ErrorCode.CAN_NOT_UPDATE_OWNER);
         }
 
-        if (member.getStatus().equals(EStatus.INACTIVE)) {
-            throw new AppException(ErrorCode.MEMBER_INACTIVE);
-        }
+
 
         member.setSubRole(status);
         memberRepository.save(member);
