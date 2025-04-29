a0bec6f4fc38b6b530c0c74095f655dbadf4530d
caleb1123 <ryy1507@gmail.com>
2025-04-29 13:07:06 +0700

    update

--- src/main/java/com/pse/tixclick/config/WebSocketConfig.java
+++ src/main/java/com/pse/tixclick/config/WebSocketConfig.java
@@ -27,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("http://localhost:5173", "https://tixclick.site")
                 .setHandshakeHandler(new DefaultHandshakeHandler() {
                     @Override
                     protected Principal determineUser(ServerHttpRequest request,
