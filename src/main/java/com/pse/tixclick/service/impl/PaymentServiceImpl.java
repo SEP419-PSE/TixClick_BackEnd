45064108fcbe49a9ea6ac733e0f734d3fb495929
caleb1123 <ryy1507@gmail.com>
2025-04-30 21:18:04 +0700

    update

--- src/main/java/com/pse/tixclick/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/pse/tixclick/service/impl/PaymentServiceImpl.java
@@ -595,6 +595,6 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     private String getBaseUrl(HttpServletRequest request) {
-        return "http://localhost:5173";
+        return "https://tixclick.site";
     }
 }
