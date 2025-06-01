fb77b1d1cf1d3076c967bf8ef408e4a457c7426f
caleb1123 <ryy1507@gmail.com>
2025-06-01 20:23:38 +0700

    fix checkin

--- src/main/java/com/pse/tixclick/repository/CheckinLogRepository.java
+++ src/main/java/com/pse/tixclick/repository/CheckinLogRepository.java
@@ -20,25 +20,37 @@ public interface CheckinLogRepository extends JpaRepository<CheckinLog, Integer>
 
     @Query(value = """
 
-            SELECT\s
-            t.ticket_name AS ticketName,
-            t.ticket_id AS ticketId,
-            COUNT(tp.ticket_purchase_id) AS totalPurchased,
-            COUNT(CASE WHEN cl.checkin_status = 'CHECKED_IN' THEN 1 END) AS checkedIn,
-            COUNT(CASE WHEN cl.checkin_status IS NULL OR cl.checkin_status = 'PENDING' THEN 1 END) AS notCheckedIn
-        FROM
-            ticket t
-        LEFT JOIN
-            ticket_purchase tp ON t.ticket_id = tp.ticket_id
-        LEFT JOIN
-            orders o ON tp.order_code = o.order_code
-        LEFT JOIN
-            checkin_log cl ON o.order_id = cl.order_id
-        WHERE
-            tp.event_activity_id = 2
-        GROUP BY\s
-            t.ticket_name, t.ticket_id;
-        
+          SELECT
+              t.ticket_name AS ticketName,
+              t.ticket_id AS ticketId,
+             \s
+              COUNT(tp.ticket_purchase_id) AS totalPurchased,
+             \s
+              COUNT(CASE WHEN cl.checkin_status = 'CHECKED_IN' THEN 1 END) AS checkedIn,
+              COUNT(CASE WHEN cl.checkin_status IS NULL OR cl.checkin_status = 'PENDING' THEN 1 END) AS notCheckedIn,
+          
+              -- Thêm cột tổng vé có status hợp lệ theo từng ticket_id
+              (
+                  SELECT COUNT(tp2.ticket_purchase_id)
+                  FROM ticket_purchase tp2
+                  WHERE tp2.event_activity_id = 2
+                    AND tp2.status IN ('PURCHASED', 'CHECKED_IN')
+                    AND tp2.ticket_id = t.ticket_id
+              ) AS totalTicket
+          
+          FROM
+              ticket t
+          LEFT JOIN
+              ticket_purchase tp ON t.ticket_id = tp.ticket_id
+          LEFT JOIN
+              orders o ON tp.order_code = o.order_code
+          LEFT JOIN
+              checkin_log cl ON o.order_id = cl.order_id
+          WHERE
+              tp.event_activity_id = 2
+          GROUP BY
+              t.ticket_name, t.ticket_id;
+          
         """, nativeQuery = true)
     List<TicketCheckinStatsProjection> getTicketCheckinStatsByEventActivityId(@Param("eventActivityId") int eventActivityId);
 
